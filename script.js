2f74d6e33e00f4c21d3da86a0b2f24010ae002e5
Lodenius <yhfe22.lodeph@folkuniversitetet.nu>
2023-03-08 09:48:22 +0100

    finished

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ function overlayOn() {
     overlayEl.style.display = 'block'; // Så overlay syns
     overlayEl.innerHTML = `
     <section class="overlay-container">
+    <button class="previous-planet navigate-btn"> ⇠ </button>
         <section class="planet-card">
             <button class="close-overlay">Tillbaka ut i rymden 🚀</button>
             <h3>${body.name}</h3>
@@ -72,15 +73,9 @@ function overlayOn() {
                 <p class="bold">MÅNAR</p>
                 <p>${body.moons.map((moon) => `${moon}`).join("  |  ")}</p>
             </section>
-            
-            <div class="divider"></div>
-            
-            <section class="navigate-planets">
-                <button class="previous-planet navigate-btn"> ⇠ </button>
-                <button class="next-planet navigate-btn"> ⇢ </button>
+
             </section>
-            
-            </section>
+        <button class="next-planet navigate-btn"> ⇢ </button>
         </section>
     `;
             
@@ -142,153 +137,3 @@ searchPlanet.addEventListener("keyup", function(event) {
         };
     };
 });
-
-
-//////////////////
-/*
-const solisURL = 'https://majazocom.github.io/Data/solaris.json';
-const planetsContainer = document.querySelector('.space-container');
-const overlayEl = document.querySelector('.space-overlay');
-const searchPlanet = document.querySelector('#input-search');
-
-let planets = [];
-let body = [];
-let planetId;
-
-// Fetcha solsystemet
-async function getSolarSystem() {
-    let solarSystem = await fetch(solisURL);
-    planets = await solarSystem.json();
-    console.log(planets);
-    renderPlanetsToUI();
-};
-
-// Rendera ut planeterna i UI
-function renderPlanetsToUI(){
-    planets.forEach(planet => {
-        let planetEl = document.createElement('div');
-        planetEl.classList.add('planet', `planet-${planet.id}`);
-        planetEl.setAttribute('id', planet.id);
-        
-        planetsContainer.appendChild(planetEl);
-        
-        planetEl.addEventListener('click', () => { // Klick funktion för att öppna overlay
-            // console.log(planet);
-            body = planet;
-            overlayOn();
-        });
-    });
-};
-getSolarSystem();
-
-// Skapa innehållet i overlay
-function overlayOn() {
-    
-    overlayEl.style.display = 'block';
-    overlayEl.innerHTML = `
-        <section class="overlay-container">
-            <section class="planet-card">
-                <button class="close-overlay">Tillbaka ut i rymden 🚀</button>
-                <h3>${body.name}</h3>
-                <h2>${body.latinName}</h2>
-                <p class="desc">${body.desc}</p>
-        
-            <div class="divider"></div>
-
-            <section class="planet-card__info">
-                <section>
-                    <p class="bold">OMKRETS</p>
-                    <p>${body.circumference} km</p>
-                </section>
-        
-                <section>
-                    <p class="bold">KM FRÅN SOLEN</p>
-                    <p>${body.distance} km</p>
-                </section>
-        
-                <section>
-                    <p class="bold">MAX TEMPERATUR</p>
-                    <p>${body.temp.day} °C </p>
-                </section>
-        
-                <section>
-                    <p class="bold">MIN TEMPERATUR</p>
-                    <p>${body.temp.night}°C </p>
-                </section>
-            </section>
-        
-            <div class="divider"></div>
-            
-            <section>
-                <p class="bold">MÅNAR</p>
-                <p>${body.moons.map((moon) => `${moon}`).join("  |  ")}</p>
-            </section>
-            
-            <div class="divider"></div>
-            
-            <section class="navigate-planets">
-                <button class="previous-planet navigate-btn"> ⇠ </button>
-                <button class="next-planet navigate-btn"> ⇢ </button>
-            </section>
-            
-            </section>
-        </section>
-            `;
-            
-    // Stäng overlay när knappen trycks
-    document.querySelector('.close-overlay').addEventListener('click', () => { 
-        overlayEl.style.display = 'none';
-    });
-      
-    // Navigation på overlayvy
-    // Föregående planet
-    document.querySelector('.previous-planet').addEventListener('click', () => {
-        // console.log(body.id - 1);
-        let planetId = body.id;
-        planetId = planetId - 1;
-
-        planets.find(planet => {
-            if (planet.id === planetId) {
-                body = planet;
-                overlayOn()
-            } else if (planetId === - 1) {
-                overlayEl.style.display = 'none';
-                // alert('Det finns inga fler himlakroppar i det här solsystemet! 🪐');
-            };
-        });
-    });
-    
-    // Nästa planet
-    document.querySelector('.next-planet').addEventListener('click', () => {
-        // console.log(body.id + 1);
-        let planetId = body.id;
-        planetId = planetId + 1;
-
-        planets.find(planet => {
-            if (planet.id === planetId) {
-                body = planet;
-                overlayOn()
-            } else if (planetId === 9) {
-                overlayEl.style.display = 'none';
-                // alert('Det finns inga fler himlakroppar i det här solsystemet! 🪐');
-            };
-        });
-    });
-};
-
-
-// Sökfunktion
-searchPlanet.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-        // console.log('Ta mig till', event.target.value);
-        let planetIndex = planets.findIndex(planet => planet.name.toLowerCase().includes(event.target.value.toLowerCase()));
-        // console.log(planetIndex);
-        if (planetIndex > -1) {
-            body = planets[planetIndex];
-            overlayOn();
-        } else {
-            alert(event.target.value + ' finns inte i det här solsystemet! 🪐');
-        }
-    }
-});
-*/
